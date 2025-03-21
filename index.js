9879e138abbed6337a91bede965c09447c1c0220
davidwcouto <david.kouto@hotmail.com>
2025-03-21 18:48:58 -0300

    Atualizando o bot

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ client.on("message_create", async (message) => {
         // Lista de palavras-chave usadas em respostas automáticas do bot
         const mensagensDoBot = [
             "📞", "💰", "⏳", "❌", "Olá!", "Digite o nome do produto",
-            "Como posso te ajudar?", "Para fazer pedido digite 2️⃣", "Digite a opção"
+            "Como posso te ajudar?", "Para fazer pedido digite 2️⃣", "Digite a opção", "⚠ Nenhum produto"
         ];
 
         // Verifica se a mensagem é uma resposta automática do bot
